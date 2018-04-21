2adf8bf9569307f4bc69fc884457b059412644c7
Jim Daehn <jdaehn@missouristate.edu>
2018-04-21 14:48:38 -0500

    Update C++ style/format to CLion default.

--- main.cpp
+++ main.cpp
@@ -8,30 +8,32 @@
 #include <iostream>
 
 class Square {
- private:
-  int side;
- public:
-  Square(int s = 1) {
-    side = s;
-  }
-  void setSide(int s) {
-    side = s;
-  }
-  int getSide() {
-    return side;
-  }
+private:
+    int side;
+public:
+    Square(int s = 1) {
+        side = s;
+    }
+
+    void setSide(int s) {
+        side = s;
+    }
+
+    int getSide() {
+        return side;
+    }
 };
 
 int main() {
-  std::cout << "An introduction to classes in C++!" << std::endl;
-  Square defaultSquare{};
-  std::cout << "defaultSquare.getSide() = " << defaultSquare.getSide() << std::endl;
-  Square initializedSquare{200};
-  std::cout << "initializedSquare.getSide() = " << initializedSquare.getSide() << std::endl;
-  std::cout << "\nAfter doubling each square's side..." << "\n" << std::endl;
-  defaultSquare.setSide(defaultSquare.getSide() * 2);
-  initializedSquare.setSide(initializedSquare.getSide() * 2);
-  std::cout << "defaultSquare.getSide() = " << defaultSquare.getSide() << std::endl;
-  std::cout << "initializedSquare.getSide() = " << initializedSquare.getSide() << std::endl;
-  return EXIT_SUCCESS;
+    std::cout << "An introduction to classes in C++!" << std::endl;
+    Square defaultSquare{};
+    std::cout << "defaultSquare.getSide() = " << defaultSquare.getSide() << std::endl;
+    Square initializedSquare{200};
+    std::cout << "initializedSquare.getSide() = " << initializedSquare.getSide() << std::endl;
+    std::cout << "\nAfter doubling each square's side..." << "\n" << std::endl;
+    defaultSquare.setSide(defaultSquare.getSide() * 2);
+    initializedSquare.setSide(initializedSquare.getSide() * 2);
+    std::cout << "defaultSquare.getSide() = " << defaultSquare.getSide() << std::endl;
+    std::cout << "initializedSquare.getSide() = " << initializedSquare.getSide() << std::endl;
+    return EXIT_SUCCESS;
 }
