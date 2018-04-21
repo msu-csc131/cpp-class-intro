4a9447ef1568048512bdab6a18249b720629203d
Jim Daehn <jdaehn@missouristate.edu>
2018-04-21 14:26:49 -0500

    Initial implementation of main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+/**
+ * Demonstration of a simple class in C++.
+ *
+ * @file main.cpp
+ * @author Jim Daehn
+ */
+#include <cstdlib>
+#include <iostream>
+
+int main() {
+  std::cout << "An introduction to classes in C++!" << std::endl;
+  return EXIT_SUCCESS;
+}
